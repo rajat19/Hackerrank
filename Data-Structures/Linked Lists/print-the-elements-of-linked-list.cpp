0873f34168a695a0bb337323335e4bfb94ee957f
rajat <rajatsri94@gmail.com>
2018-07-07 11:38:14 +0530

    initial ds and fp commit

---
+++ Data-Structures/Linked Lists/print-the-elements-of-linked-list.cpp
@@ -0,0 +1,18 @@
+/*
+  Print elements of a linked list on console 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+void Print(Node *head)
+{
+    Node *p = head;
+    while(p != NULL) {
+        cout<<p->data << endl;
+        p = p->next;
+    }
+}
