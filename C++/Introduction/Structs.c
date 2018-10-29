9ddbe9676657d83c39fa85fe2af720acea1d20b1
rahulambastha <42257592+rahulambastha@users.noreply.github.com>
2018-10-30 01:15:44 +0530

    Create Structs.c

---
+++ C++/Introduction/Structs.c
@@ -0,0 +1,31 @@
+/*
+  Problem link: https://www.hackerrank.com/challenges/c-tutorial-struct/problem
+  Problem Name: Structs | HackerRank
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*
+    add code for struct here.
+*/
+
+struct Student {
+    int age;
+    string first_name;
+    string last_name;
+    int standard;
+};
+
+int main() {
+    Student st;
+    
+    cin >> st.age >> st.first_name >> st.last_name >> st.standard;
+    cout << st.age << " " << st.first_name << " " << st.last_name << " " << st.standard;
+    
+    return 0;
+}
