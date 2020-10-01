980c26015f18868311d5e91cb534798f383bc032
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2020-10-01 12:30:26 +0530

    Update c-tutorial-basic-data-types.cpp

--- C++/Introduction/c-tutorial-basic-data-types.cpp
+++ C++/Introduction/c-tutorial-basic-data-types.cpp
@@ -1,6 +1,21 @@
+//HeaderFiles
 #include <iostream>
 #include <cstdio>
 using namespace std;
 
-int main() { int a; long int b; long long int c; char d; float e; double f; scanf("%d %ld %lld %c %f %lf",&a,&b,&c,&d,&e,&f); printf("%d\n%ld\n%lld\n%c\n%f\n%lf",a,b,c,d,e,f);
-            return 0;}
+//main function
+int main()
+{
+    //variable declaration
+    int var_1;
+    long int var_2;
+    long long int var_3;
+    char var_4;
+    float var_5;
+    double var_6;
+    //input values from user
+    cin>>var_1>>var_2>>var_3>>var_4>>var_5>>var_6;
+    //print the output in STD_OUTPUT
+    cout<<var_1<<endl<<var_2<<endl<<var_3<<endl<<var_4<<endl<<var_5<<endl<<var_6;
+    return 0;
+}
