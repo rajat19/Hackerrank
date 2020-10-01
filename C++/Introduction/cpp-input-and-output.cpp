d5f1ff33a1d087cbebddd172f7c816083b8fad0b
Abhishek Sharma <58878438+imgeekabhi@users.noreply.github.com>
2020-10-01 11:16:12 +0530

    Update cpp-input-and-output.cpp

--- C++/Introduction/cpp-input-and-output.cpp
+++ C++/Introduction/cpp-input-and-output.cpp
@@ -4,11 +4,15 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
+//main function
+int main()
+{
+    int input_1, input_2, input_3;
 
+    //The "extraction operator" >> is the one we usually use for input
+    cin>>input_1>>input_2>>input_3;
 
-int main() {
-    int i,j,k;
-    cin>>i>>j>>k;
-    cout<<(i+j+k);
+    //The "insertion operator" << is the one we usually use for output
+    cout<<(input_1 + input_2 + input_3);
     return 0;
 }
