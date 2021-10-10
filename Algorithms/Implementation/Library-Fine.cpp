3ae2384e423eef00c2e50f3c8e8463d9eabe31a6
Mayank Pathak <mayankpathak4513@gmail.com>
2021-10-10 16:08:19 +0530

    Create Library-Fine.cpp
    Added new problem with its solution

---
+++ Algorithms/Implementation/Library-Fine.cpp
@@ -0,0 +1,33 @@
+/*Code written by MAYANK PATHAK*/
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n,k,b,s=0;
+    cin>>n>>k;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+        s+=a[i];
+    }
+    cin>>b;
+    int l=s-a[k];
+
+    if((l/2)==b)
+    {
+        cout<<"Bon Appetit";
+    }
+    else
+    {
+        cout<<b-(l/2);
+    }
+    return 0;
+
+}
+
+
+
