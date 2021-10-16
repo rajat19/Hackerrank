c0b1e17fb17682ca33ed7465800a45121c5c6af4
Akash Sen <53941291+2207akash@users.noreply.github.com>
2021-10-16 09:35:22 +0530

    Added solution to problem Code Meal
    Mathematics (Algebra) section: Problem Code Meal

---
+++ Mathematics/Algebra/combo-meal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int tc;
+    cin >> tc;
+    while(tc--) {
+        int b, s, c;
+        cin >> b >> s >> c;
+        cout << b+s-c << endl;
+    }
+    return 0;
+}
