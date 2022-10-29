5cc0ce3043dfd3cf36ab2d29aaa0264e8258a77c
Dushyant100 <dushyant01agra@gmail.com>
2022-10-30 00:10:01 +0530

    add equal solution
    Signed-off-by: Dushyant100 <dushyant01agra@gmail.com>

---
+++ Algorithms/Dynamic-Programming/equal.cpp
@@ -0,0 +1,100 @@
+//hacker rank question link : https://www.hackerrank.com/challenges/equal/problem
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+
+int equal(vector<int> arr) {
+    auto min_ = *min_element(arr.begin() , arr.end());
+    auto ans = numeric_limits<int>::max();
+    for(auto i=0; i<5 ; i++){
+        auto operations = [min_,i](auto init , auto j){
+            auto t = j - (min_ - i);
+            return init + t/5 + t%5/2 + t%5%2;
+        };
+        ans = min (ans , accumulate (arr.begin () , arr.end (), 0 , operations));
+    };
+    return ans;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        int n = stoi(ltrim(rtrim(n_temp)));
+
+        string arr_temp_temp;
+        getline(cin, arr_temp_temp);
+
+        vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+        vector<int> arr(n);
+
+        for (int i = 0; i < n; i++) {
+            int arr_item = stoi(arr_temp[i]);
+
+            arr[i] = arr_item;
+        }
+
+        int result = equal(arr);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
